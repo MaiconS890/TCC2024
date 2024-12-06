95050c7d3d9bf7beeb3006d00085bd9cc87b2319
Maicon Soares de Souza <153025257+MaiconS890@users.noreply.github.com>
2024-12-06 18:03:13 -0300

    Add files via upload
    vitor-js do modal arrumado e css também

---
+++ 06.12/TCC2024/JS/botao.js
@@ -0,0 +1,20 @@
+function redirecionarDoce() {
+   
+    window.location.href = "../HTML/receitaPrincipal.html";
+}
+function redirecionarSalgado() {
+    
+    window.location.href = "../HTML/receitaPrincipal2.html";
+}
+function redirecionarAjuda() {
+    
+    window.location.href = "../HTML/receitaAjuda.html";
+}
+function voltarAjuda() {
+    
+    window.location.href = "../index.html";
+}
+function voltarInicio() {
+    
+    window.location.href = "../index.html";
+}
